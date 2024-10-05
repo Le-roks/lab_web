c23af349909c4540b54a190e8d5f8ce521ac5949
Valeria Tereshchenko <tevase2002@gmail.com>
2024-10-05 18:41:59 +0300

    new com

--- lab1/class.php
+++ lab1/class.php
@@ -1,26 +1,40 @@
 <?php class Coor
-{  //creating Coor class
-
-    private $name;  //private variable name
-
+{
+    private $name;
     function Getname()
-    {  //function for getting “name” variable
+    {
 
-        return $this->name; //function return value of “name” variable
+        echo $this->name;
 
     }
 
-    function Setname($text)  //function for setting “name” variable
+    function Setname($text)
     {
 
-        $this->name = $text;  //setting “name” by “text” 
+        $this->name = $text;
 
     }
 
 }
 
-$object = new Coor;  //creating new object of “Coor” class
+$works = array();//creating a new array
 
-$object->Setname("Nick"); //setting “name”
+$works[0] = new Coor();//writing a “Coor” object in array
 
-echo $object->Getname(); //printing object name ?>
+$works[0]->Getname() = " Nick ";//set a name
+
+$works[1] = new Coor();
+
+$works[1]->Getname() = " Nick 1";
+
+$works[2] = new Coor();
+
+$works[2]->Getname() = " Nick 2";
+
+for ($i = 0; $i < 3; $i++) {
+    echo $works[$i]->Getname();
+}
+
+//circle with printing names of objects in array
+
+?>
