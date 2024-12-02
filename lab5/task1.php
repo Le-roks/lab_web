558ba8612f9dd3f37d27d89dd2f78c864bf3ebf9
Valeria Tereshchenko <tevase2002@gmail.com>
2024-12-02 16:44:39 +0200

    task1

---
+++ lab5/task1.php
@@ -0,0 +1,39 @@
+<?php class someClass
+{
+    protected static $_instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$_instance === null) {
+
+            someClass::$_instance = new someClass();
+        }
+
+        return self::$_instance;
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    private function __wakeup()
+    {
+
+    }
+}
+
+// Тестуємо патерн Singleton
+$obj1 = someClass::getInstance();  // Перший екземпляр
+$obj2 = someClass::getInstance();  // Другий екземпляр (отриманий через Singleton)
+
+if ($obj1 === $obj2) {
+    echo "Один і той же об'єкт!";
+} else {
+    echo "Різні об'єкти!";
+}
+?>
